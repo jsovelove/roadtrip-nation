0c66b09fcb6f943938ae944156dbd2ebd7ec19cb
Jack Sovelove <soveloj@wwu.edu>
2025-06-23 14:47:57 -0700

    fix base URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/roadtrip-nation/', // Set base URL for GitHub Pages
+  base: '/', // Set base URL for GitHub Pages (root domain)
   define: {
     global: 'globalThis',
   },
